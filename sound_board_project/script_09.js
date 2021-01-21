10a5312de48fbc410734c423096a05289ed8ca31
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-01-21 23:46:18 +0300

    sound board finish

---
+++ sound_board_project/script_09.js
@@ -0,0 +1,22 @@
+const sounds = ["ovation", "horse", "sonar", "yip", "ksilofon", "drum"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById("buttons").appendChild(btn);
+});
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
